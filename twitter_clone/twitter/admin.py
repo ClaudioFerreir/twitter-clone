76fb785dfc3da8bca0305ea3d8cdb89c84e4ded9
ClaudioFerreir <claudioferreirajr@hotmail.com>
2024-05-20 11:00:19 -0300

    chore: profile user inline add

--- twitter_clone/twitter/admin.py
+++ twitter_clone/twitter/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group, User
+from .models import Profile
 
 # Register your models here.
 # Unregister Groups
 admin.site.unregister(Group)
+
+
+# Mix Profile info into User Info
+class ProfileInLine(admin.StackedInline):
+    model = Profile
 
 
 # Extend User Model
@@ -11,10 +17,12 @@ class UserAdmin(admin.ModelAdmin):
     model = User
     # Just display username fields on admin page
     fields = ['username']
+    inlines = [ProfileInLine]
 
 
 # Unregister initial User
 admin.site.unregister(User)
 
-# Reregister User
+# Reregister User and Profile
 admin.site.register(User, UserAdmin)
+#admin.site.register(Profile)
--- twitter_clone/twitter/models.py
+++ twitter_clone/twitter/models.py
@@ -8,3 +8,7 @@ class Profile(models.Model):
                                      related_name="followed_by",
                                      symmetrical=False,
                                      blank=True)
+
+    def __str__(self):
+        return self.user.username
+
