5d413835430b836f732efef25ad7d29c47d138a8
ClaudioFerreir <claudioferreirajr@hotmail.com>
2024-05-23 22:17:54 -0300

    feat: profile page with followers add

--- twitter_clone/twitter/views.py
+++ twitter_clone/twitter/views.py
@@ -14,3 +14,12 @@ def profile_list(request):
     else:
         messages.success(request, 'You Must Be Logged In To View this Page')
         return redirect('home')
+
+
+def profile(request, pk):
+    if request.user.is_authenticated:
+        profile = Profile.objects.get(user_id=pk)
+        return render(request, 'profile.html', {'profile': profile})
+    else:
+        messages.success(request, 'You Must Be Logged In To View this Page')
+        return redirect('home')
