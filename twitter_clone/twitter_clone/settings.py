73518a5f7c7d3d38911132fd55e3fc77d3260f11
ClaudioFerreir <claudioferreirajr@hotmail.com>
2024-10-08 22:47:05 -0300

    fix: settings db mySQL

--- twitter_clone/twitter_clone/settings.py
+++ twitter_clone/twitter_clone/settings.py
@@ -43,7 +43,7 @@ if os.getenv('USE_MYSQL', 'False').lower() == 'true':
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.mysql',
-            'NAME': os.getenv('DB_NAME'),
+            'NAME': os.getenv('DB_NAME_MYSQL'),
             'USER': os.getenv('DB_USER'),
             'PASSWORD': os.getenv('DB_PASSWORD'),
             'HOST': os.getenv('DB_HOST', 'localhost'),
